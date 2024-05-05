034294441699b1adafb8fe2c0eb7bedbb367d60a
Adrian Z <sylzyg555@interia.pl>
2024-05-05 18:56:08 +0200

    CORS problem / added simple get req

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { Note } from "./models/note";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [notes, setNotes] = useState<Note[]>([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await fetch("http://localhost:5000/api/notes", {
+          method: "GET",
+        }).then(async (response) => {
+          const notes = await response.json();
+          setNotes(notes);
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchData();
+  }, [notes]);
+  return(
+  <div className=" text-white">
+    {JSON.stringify(notes)}
+  </div>
   )
 }
 
-export default App
+export default App;
